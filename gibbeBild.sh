656f20e6b21597afa535cef7bf3a120fea47f69b
ko89 <ko89@posteo.de>
2014-08-13 21:23:02 +0200

    Convenience Mode
    If only the url and no other arguments or parameters are supplied the
    script automatically loads the highest quality image. (Quick hack)

--- gibbeBild.sh
+++ gibbeBild.sh
@@ -6,6 +6,7 @@
 # Usage: $ ./gibbeBild -u [twitter-URL] -s [small/large/orig(=default)]
 # ... downloads pictures directly into the home-folder.
 # Todo: implement folder structure for auto-sorting.
+# Todo: convenience mode (./gibbeBild.sh [twitter-URL]) could be handled nicer
 
 size=":orig"
 while getopts u:s: options 2>/dev/null; do # error > /dev/null
@@ -17,6 +18,16 @@ while getopts u:s: options 2>/dev/null; do # error > /dev/null
 done
 
 shift $(( OPTIND - 1 )) # important for parameters!
+
+if [ -z $url ]; then
+	if [ -n $1 ]; then
+		echo "Url parameter (-u) not found. Assuming url is the only argument"
+		url=$1
+	else
+		echo "No url supplied!"
+		exit
+	fi
+fi
 
 echo "twitterurl: $url"
 address="https:$(curl $url | grep 'https://pbs.twimg.com/media/' | sed q | cut -d':' -f 2)$size"
